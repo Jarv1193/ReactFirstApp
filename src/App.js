4a158eadfa403e70a45f47bc235fad3268af1ea0
jarv1993 <pepe_antonio_rosales@hotmail.com>
2018-03-16 18:35:04 -0600

    Modificando los mensajes originales de react

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Moviendole a React con Git y Git Hub</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload Really.
+          Viendo los cambios en tiempo real.
         </p>
       </div>
     );
